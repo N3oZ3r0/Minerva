07229f59227fd6dc8c225e36d1ce0317c36f72ed
Your Name <45666661+N3oZ3r0@users.noreply.github.com>
2021-05-27 07:47:27 +0200

    Footer e iconos de boostrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { SplashScreenComponent } from './splash-screen/splash-screen.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { SwiperComponent } from './swiper/swiper.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { CarouselComponent } from './carousel/carousel.component';
     NavbarComponent,
     FooterComponent,
     SplashScreenComponent,
-    CarouselComponent
+    CarouselComponent,
+    SwiperComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/swiper/swiper.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+// import Swiper core and required modules
+import SwiperCore, { Pagination } from "swiper/core";
+
+// install Swiper modules
+SwiperCore.use([Pagination]);
+
+@Component({
+  selector: 'app-swiper',
+  templateUrl: './swiper.component.html',
+  styleUrls: ['./swiper.component.css']
+})
+export class SwiperComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
