bb873eac9a404910a48804d553cc8741eb5e844b
Your Name <45666661+N3oZ3r0@users.noreply.github.com>
2021-05-27 08:22:48 +0200

    navegabilidada footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent} from './home/home.component';
+import { FilesComponent} from './files/files.component';
+import { LikeComponent} from './like/like.component';
+import { BarsComponent} from './bars/bars.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch:"full"},
+  { path: 'favoritos', component: LikeComponent, pathMatch:"full"},
+  { path: 'apuntes', component: FilesComponent, pathMatch:"full"},
+  { path: 'estadisticas', component: BarsComponent, pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { FooterComponent } from './footer/footer.component';
 import { SplashScreenComponent } from './splash-screen/splash-screen.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { SwiperComponent } from './swiper/swiper.component';
+import { HomeComponent } from './home/home.component';
+import { LikeComponent } from './like/like.component';
+import { FilesComponent } from './files/files.component';
+import { BarsComponent } from './bars/bars.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,11 @@ import { SwiperComponent } from './swiper/swiper.component';
     FooterComponent,
     SplashScreenComponent,
     CarouselComponent,
-    SwiperComponent
+    SwiperComponent,
+    HomeComponent,
+    LikeComponent,
+    FilesComponent,
+    BarsComponent
   ],
   imports: [
     BrowserModule,
